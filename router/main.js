59b39791102752ebcad54e1daf2ec1b53ca5ba58
jaydenchuljinlee <cheoljin1234@naver.com>
2019-09-03 22:35:28 +0900

    body-parser,ejs,express-session

--- router/main.js
+++ router/main.js
@@ -1,8 +1,9 @@
-module.exports = function(app) {
-    app.get('/',function(req,res) {
-        res.render('index.html');
-    });
-    app.get('/about',function(req,res) {
-        res.render('about.html');
-    });
+module.exports = function(app, fs)
+{
+     app.get('/',function(req,res){
+         res.render('index', {
+             title: "MY HOMEPAGE",
+             length: 5
+         })
+     });
 }
