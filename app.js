4d4935a4c1ceeb2692ada2604297fec7dfba75e1
jaydenchuljinlee <cheoljin1234@naver.com>
2019-09-04 14:44:45 +0900

    mongoDB

---
+++ app.js
@@ -0,0 +1,35 @@
+//load packages
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+
+//configure app to use bodyParser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//configure server port
+var port = process.env.port || 8080;
+
+//configure router
+var router = require('./routes')(app,Book);
+
+//connect to mongoDB server
+var db = mongoose.connection;
+db.on('error',console.error);
+db.once('open',function() {
+    //connected to mongoDB server
+    console.log("Connected to mongoDB server");
+});
+
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
+
+//define model
+var Book = require('./models/book');
+
+//run server
+var server = app.listen(port,function() {
+    console.log('Express server has started on port' + port);
+});
+
+
